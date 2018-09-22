edaaf0e8b9bead32ff2769fba5707e2e1e22ce26
Carlos Pereira B <gpereirab89@gmail.com>
2018-09-21 22:00:19 -0600

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//choose dev or prod
+if (process.env.NODE_ENV === 'production') {
+    //We are in production
+    module.exports = require('./prod');
+} else {
+    //We are in development
+    module.exports = require('./dev');
+}
